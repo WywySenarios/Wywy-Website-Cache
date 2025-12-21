5a62f5e00a7a305d8d09088f4d30a4add7948e6b
WywySenarios <wywyericzhu@gmail.com>
2025-12-21 15:06:06 -0500

    Add Dummy Data to POST.py

--- helpers/POST.py
+++ helpers/POST.py
@@ -7,12 +7,32 @@ session.get("http://127.0.0.1:8000/main/")
 csrf_token = session.cookies.get("csrftoken")
 
 headers = {
-    "X-CSRFToken": csrf_token
+    "X-CSRFToken": csrf_token,
+    "Content-Type": "application/json",
 }
 
 response = session.post(
-    "http://127.0.0.1:8000/main/",
-    json={"hello": "world"},
+    "http://127.0.0.1:8000/main/WywyWebsite/daily",
+    json={
+        "Date":"'0001-01-01T01:00:00'",
+        "Work Ethic":0,
+        "Time Efficiency":0,
+        "Time Efficiency_comments": "heya",
+        "Happiness":0,
+        "Awareness":0,
+        "Sleep Quality":0,
+        "Sleep Behaviour":0,
+        "Bedtime":"'03:30:00.000'",
+        "Awakening":"'12:00:00.000'",
+        "Productivity":"",
+        "Social":"",
+        "Exercise":"",
+        "Computer":"",
+        "Phone":"",
+        "Awake":"",
+        "Light Sleep":"",
+        "Deep Sleep":"",
+        "Record Time":0},
     headers=headers
 )
 
