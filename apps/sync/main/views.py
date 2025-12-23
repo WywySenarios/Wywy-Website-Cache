ea0ee308796ff5519e3eb0170ee9f2fb50e277a1
WywySenarios <wywyericzhu@gmail.com>
2025-12-23 12:31:01 -0500

    Store POST Data
    * store data with the correct ID relative to the main production database
    * create the auto sync logic
    * TODO sync correctly

--- apps/sync/main/views.py
+++ apps/sync/main/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
-from django.http import HttpResponse, JsonResponse, HttpRequest, HttpResponseBadRequest
+from django.http import HttpResponse, JsonResponse, HttpRequest, HttpResponseBadRequest, HttpResponseServerError
 from typing import List
 import yaml
 import json
 import re
+import psycopg
+from psycopg import sql
+import threading
+from os import environ as env
+import requests
+import uuid
 
 def to_snake_case(target: str) -> str:
     """Attempts to convert from regular words/sentences to snake_case. This will not affect strings already in underscore notation. (Does not work with camelCase)
@@ -34,6 +40,39 @@ def to_lower_snake_case(target: str) -> str:
     
     return output[:-1] # remove trailing underscore with "[:-1]"
 
+def auto_sync(stop_event: threading.Event) -> None:
+    # automatic sync interval in minutes
+    auto_sync_interval: float = float(env.get("AUTOSYNC_INTERVAL", 5))
+    
+    if not auto_sync_interval > 0:
+        auto_sync_interval = 5
+    
+    print(f"Auto-sync is set to sync every {auto_sync_interval} minutes.")
+    
+    while True:
+        # wait until automatic sync interval or until interupted
+        if stop_event.wait(timeout=(auto_sync_interval*60)):
+            print("Sync requested via interupt...")
+            stop_event.clear()
+        else:
+            print("Automatically syncing...")
+        
+        sync()
+
+def sync() -> None:
+    pass
+
+def get_next_id(table_name: str) -> int:
+    with open("/run/secrets/admin", "r") as f:
+        response = requests.get(config["referenceUrls"]["db"] + "/wywywebsite/" + table_name + "/get_next_id", cookies={
+            "username": "admin",
+            "password": f.read()
+        }, timeout=5)
+        response.raise_for_status()
+        
+        return int(response.text)
+
+# START - Global variables
 # datatype checking functions
 DATATYPE_CHECK: dict = {
     "int": lambda x: isinstance(x, int),
@@ -43,8 +82,8 @@ DATATYPE_CHECK: dict = {
     "string": lambda x: x is not None,
     "str": lambda x: x is not None,
     "text": lambda x: x is not None,
-    "bool": lambda x: x in ("true", "false", True, False),
-    "boolean": lambda x: x in ("true", "false", True, False),
+    "bool": lambda x: str(x).capitalize() in ("TRUE", "FALSE"),
+    "boolean": lambda x: str(x).capitalize() in ("TRUE", "FALSE"),
 
     # yyyy-mm-dd (1–4 digit year)
     "date": lambda x: isinstance(x, str) and re.fullmatch(r"\'[0-9]{1,4}-[0-9]{2}-[0-9]{2}\'", x) is not None,
@@ -65,6 +104,36 @@ DATATYPE_CHECK: dict = {
         r"|[0-9]{6}"
         r"|[0-9]{6}\.[0-9]{1,6})\'", x
     ) is not None
+}
+
+DEFAULT_VALUES = {
+    "int": 0,
+    "integer": 0,
+    "float": 0.0,
+    "number": 0.0,
+    "string": "",
+    "str": "",
+    "text": "",
+    "bool": False,
+    "boolean": False,
+    "date": "'0001-01-01'",
+    "time": "'01:00:00'",
+    "timestamp": "'0001-01-01T01:00:00'"
+}
+
+REQUIRES_QUOTATION = {
+    "int": False,
+    "integer": False,
+    "float": False,
+    "number": False,
+    "string": True,
+    "str": True,
+    "text": True,
+    "bool": False,
+    "boolean": False,
+    "date": True,
+    "time": True,
+    "timestamp": True
 }
 
 # peak at config
@@ -88,6 +157,13 @@ databases: dict = {
     for db in config["data"]
 }
 
+STOP_EVENT: threading.Event = threading.Event()
+
+AUTO_SYNC_THREAD: threading.Thread = threading.Thread(target=auto_sync, args=(STOP_EVENT,))
+AUTO_SYNC_THREAD.start()
+
+# END - Global variables
+
 @ensure_csrf_cookie
 def index(request: HttpRequest) -> HttpResponse:
     if request.method == "POST" and request.content_type == "application/json":
@@ -104,6 +180,12 @@ def index(request: HttpRequest) -> HttpResponse:
         
         # load in body
         data = json.loads(request.body)
+        
+        # make a copy of data with snake_cased keys
+        f_data = {
+            to_lower_snake_case(key): data[key]
+            for key in data
+        }
         
         if data == None:
             return HttpResponseBadRequest()
@@ -142,7 +224,73 @@ def index(request: HttpRequest) -> HttpResponse:
         if not valid_request:
             return HttpResponseBadRequest()
         # END - validate schema
+        
+        # store data
+        # https://en.wikipedia.org/wiki/Two-phase_commit_protocol
+        
+        
+        # we need our ID to match the production db's ID.
+        # if our DB currently does not have any entries, we need to copy the production DB's next ID. Assume, since there is only one user who can commit data, that this ID is accurate.
+        # fetch the production DB's next ID.
+        next_id: int = get_next_id(table_name)
+        if not next_id:
+            return HttpResponseServerError()
+        
+        
+        with psycopg.connect(
+            dbname=db_name,
+            user=env.get("POSTGRES_USER", "postgres"),
+            password=env.get("POSTGRES_PASSWORD", "password"),
+            host="wywywebsite-cache_database",
+            port=env.get("POSTGRES_PORT", 5433)
+        ) as data_conn, psycopg.connect(
+            dbname="info",
+            user=env.get("POSTGRES_USER", "postgres"),
+            password=env.get("POSTGRES_PASSWORD", "password"),
+            host="wywywebsite-cache_database",
+            port=env.get("POSTGRES_PORT", 5433)
+        ) as info_conn:
+            
+            # cols_string: str = "id,"
+            # values_string: str = f"{next_id},"
+            cols: List[str] = ["id"]
+            values: List = [next_id]
+            
+            # populate column names & insert values
+            for col_name in table["schema"]:
+                # cols_string += str(col_name) + ","
+                cols.append(col_name)
+                
+                if col_name in f_data:
+                    # if REQUIRES_QUOTATION[table["schema"][col_name]["datatype"]] and :
+                    #     values_string += f"'{f_data[col_name]}'"
+                    # match (table["schema"][col_name]["datatype"]):
+                    #     # case "str", "string", "text":
+                    #     #     values_string += f"'{f_data[col_name]}'"
+                    #     case "bool", "boolean":
+                    #         values_string += str(f_data[col_name]).capitalize()
+                    #     case _:
+                    #         values_string += str(f_data[col_name])
+                    values.append(f_data[col_name])
+                else:
+                    match(table["schema"][col_name]["datatype"]):
+                        case "str", "string", "text":
+                            # values_string += "''"
+                            values.append("")
+                        case _:
+                            # values_string += "NULL"
+                            values.append(None)
+                # values_string += ","
+            
+            data_conn.execute(sql.SQL("INSERT INTO {table} ({fields}) VALUES({placeholders})").format(table=sql.Identifier(table_name), fields=sql.SQL(', ').join(map(sql.Identifier, cols)),placeholders=sql.SQL(', ').join(sql.Placeholder() * len(values))), values)
+            # data_conn.execute("INSERT into %s (%s) VALUES (%s);", (table_name, cols_string[:-1], values_string[:-1],))
+            info_conn.execute("INSERT INTO sync_status (table_name, sync_timestamp, status) VALUES (%s, NULL, NULL);", (table_name, ))
+        
+        # @TODO recovery
+        
+        # queue a sync
+        STOP_EVENT.set()
 
-        return JsonResponse({"ok": True})
+        return HttpResponse()
     
     return JsonResponse({"detail": "csrf cookie set"})
