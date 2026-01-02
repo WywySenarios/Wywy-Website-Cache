45d860dbb0f1efdee76408795621462e141309f9
WywySenarios <wywyericzhu@gmail.com>
2026-01-02 16:56:34 -0500

    Add utils.py

--- apps/sync/main/views.py
+++ apps/sync/main/views.py
@@ -13,33 +13,7 @@ from os import environ as env
 import requests
 import datetime
 
-def to_snake_case(target: str) -> str:
-    """Attempts to convert from regular words/sentences to snake_case. This will not affect strings already in underscore notation. (Does not work with camelCase)
-    @param target
-    @return Returns underscore notation string. e.g. "hi I am Wywy" -> "hi_I_am_Wywy"
-    """
-    stringFrags: List[str] = re.split(r"[\.\ \-]", target)
-    
-    output: str = ""
-    
-    for i in stringFrags:
-        output += i + "_"
-    
-    return output[:-1] # remove trailing underscore with "[:-1]"
-
-def to_lower_snake_case(target: str) -> str:
-    """Attempts to convert from regular words/sentences to lower_snake_case. This will not affect strings already in underscore notation. (Does not work with camelCase)
-    @param target
-    @return Returns lower_snake_case string. e.g. "hi I am Wywy" -> "hi_i_am_wywy"
-    """
-    stringFrags: List[str] = re.split(r"[\.\ \-]", target)
-    
-    output: str = ""
-    
-    for i in stringFrags:
-        output += i.lower() + "_"
-    
-    return output[:-1] # remove trailing underscore with "[:-1]"
+from apps.sync.utils import to_lower_snake_case
 
 def auto_sync(sync_event: threading.Event) -> None:
     # automatic sync interval in minutes
