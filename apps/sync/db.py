75a9e765dfa0fefda1fd6cc109670687a80778e6
WywySenarios <wywyericzhu@gmail.com>
2026-01-13 14:30:30 -0500

    Centralize sync & database access
    * Still need to remove some cursors and connections, but most of the database logic is being loaded off of mainc/views.py!

---
+++ apps/sync/db.py
@@ -0,0 +1,80 @@
+import psycopg
+from psycopg import sql
+import requests
+from typing import List
+
+
+def get_next_id(db_name: str, table_name: str) -> int:
+    with open("/run/secrets/admin", "r") as f:
+        response = requests.get(config["referenceUrls"]["db"] + "/" + db_name + "/" + table_name + "/get_next_id", cookies={
+            "username": "admin",
+            "password": f.read()
+        }, timeout=5)
+        response.raise_for_status()
+        
+        return int(response.text)
+
+def store_entry(data_conn, info_conn, item: dict, schema: dict, target_database_name: str, target_table_name: str, target_parent_table_name: str, target_table_type: str, tagging = False) -> str | None:
+    """Stores an entry in both the respective data table and the info/sync table.
+
+    Args:
+        data_conn (_type_): Connection to the target database.
+        info_conn (_type_): Connection to the info database.
+        item (dict): The item whose data will be entere.
+        schema (dict): The column schema corresponding to the entry.
+        taregt_database_name (str): The name of the target database.
+        target_table_name (str): The name of the target table.
+        target_parent_table_name (str): The name of the target table's parent.
+        target_table_type (str): The target table's type.
+        tagging (bool, optional): _description_. Defaults to False.
+
+    Raises:
+        ValueError: When a schema column is missing from the given entry to record.
+
+    Returns:
+        str | None: Returns a string with an error message or None if there was no error.
+    """
+    # we need our ID to match the production db's ID.
+    # if our DB currently does not have any entries, we need to copy the production DB's next ID. Assume, since there is only one user who can commit data, that this ID is accurate.
+    # fetch the production DB's next ID.
+    # apparently ts is unsafe (with incrementation being different behind the scenes for everyone)
+    next_id_cur = data_conn.execute(sql.SQL("SELECT COALESCE(MAX(id), 0) + 1 AS next_id FROM {table};").format(table=sql.Identifier(target_table_name)))
+    next_id_in_house: int | None = next(next_id_cur)[0]
+    next_id: int
+    if not next_id_in_house:
+        next_id = get_next_id(target_database_name, target_table_name)
+        if not next_id:
+            return HttpResponseServerError()
+    else:
+        next_id = next_id_in_house
+    next_id_cur.close()
+    
+    cols: List[str] = ["id"]
+    values: List = [next_id]
+    
+    # populate column names & insert values
+    for col_name in schema:
+        cols.append(col_name)
+        
+        if col_name in item:
+            # if REQUIRES_QUOTATION[table["schema"][col_name]["datatype"]] and :
+            #     values_string += f"'{item[col_name]}'"
+            # match (table["schema"][col_name]["datatype"]):
+            #     # case "str", "string", "text":
+            #     #     values_string += f"'{item[col_name]}'"
+            #     case "bool", "boolean":
+            #         values_string += str(item[col_name]).capitalize()
+            #     case _:
+            #         values_string += str(item[col_name])
+            values.append(item[col_name])
+        else:
+            raise ValueError(f"Column name {col_name} is not within the schema.")
+    
+    # check for primary tag column
+    if tagging:
+        cols.append("primary_tag")
+        values.append(item["primary_tag"])
+
+    # record the main entry
+    data_conn.execute(sql.SQL("INSERT INTO {table} ({fields}) VALUES({placeholders});").format(table=sql.Identifier(target_table_name), fields=sql.SQL(', ').join(map(sql.Identifier, cols)),placeholders=sql.SQL(', ').join(sql.Placeholder() * len(values))), values).close()
+    info_conn.execute("INSERT INTO sync_status (table_name, parent_table_name, table_type, db_name, entry_id, sync_timestamp, status) VALUES (%s, %s, %s, %s, %s, NULL, NULL);", (target_table_name, target_parent_table_name, target_table_type, target_database_name, next_id)).close()
---
+++ apps/sync/sync_app.py
@@ -0,0 +1,127 @@
+# @TODO fix multithreading database transaction issues >:(
+import threading
+import yaml
+import requests
+import datetime
+from os import environ as env
+import psycopg
+from psycopg.rows import dict_row
+from psycopg import sql
+from typing import List, Literal
+
+from utils import to_lower_snake_case, get_env_int
+from schema import check_entry, databases
+
+with open("config.yml", "r") as file:
+    config = yaml.safe_load(file)
+
+SYNC_VERBOSITY = get_env_int("SYNC_VERBOSITY", 0)
+
+def auto_sync(sync_event: threading.Event) -> None:
+    # automatic sync interval in minutes
+    auto_sync_interval: float = float(env.get("AUTOSYNC_INTERVAL", 5))
+    
+    if not auto_sync_interval > 0:
+        auto_sync_interval = 5
+    
+    print(f"Auto-sync is set to sync every {auto_sync_interval} minutes.")
+    
+    while True:
+        # wait until automatic sync interval or until interupted
+        if sync_event.wait(timeout=(auto_sync_interval*60)):
+            # print("Sync requested via interupt...")
+            sync_event.clear()
+        else:
+            pass
+            # print("Automatically syncing...")
+        
+        sync()
+
+def sync() -> None:
+    with psycopg.connect(
+            dbname="info",
+            user=env.get("POSTGRES_USER", "postgres"),
+            password=env.get("POSTGRES_PASSWORD", "password"),
+            host="wywywebsite-cache_database",
+            port=env.get("POSTGRES_PORT", 5433)
+        ) as info_conn:
+        # select all targets that need syncing (failed, not synced yet (NULL)) (do not select mismatch for now)
+        targets_cur = info_conn.execute("SELECT id, table_name, parent_table_name, table_type, db_name, entry_id, status FROM sync_status WHERE status IN ('failed') OR status IS NULL;")
+        
+        num_successes = 0
+        num_failures = 0
+        for target in targets_cur.fetchall():
+            sync_status_id = target[0]
+            table_name: str = target[1]
+            parent_table_name: str = target[2]
+            table_type: str = target[3]
+            db_name: str = target[4]
+            target_id: str = target[5]
+            
+            # get the information relating to the target
+            target_record_conn = psycopg.connect(
+                dbname=db_name,
+                user=env.get("POSTGRES_USER", "postgres"),
+                password=env.get("POSTGRES_PASSWORD", "password"),
+                host="wywywebsite-cache_database",
+                port=env.get("POSTGRES_PORT", 5433), row_factory=dict_row
+            )
+            target_record_cur = target_record_conn.execute(sql.SQL("""
+                                    SELECT * FROM {table} WHERE "id"=%s;
+                                    """).format(table=sql.Identifier(table_name)), (target_id,))
+            
+            # @TODO check if the target already exists
+            
+            # contact sql-receptionist and ask for a record addition
+            # @TODO sql-receptionist should reject problematic id keys
+            payload = dict(next(target_record_cur))
+            for k, v in payload.items():
+                if v is None:
+                    print(f"Sync failed. Anomalous item: ({db_name}/{table_name} ({parent_table_name})): {payload}")
+                    return
+                elif isinstance(v, datetime.datetime) or isinstance(v, datetime.date) or isinstance(v, datetime.time):
+                    payload[k] =  f"'{v.isoformat()}'"
+                elif isinstance(v, str):
+                    payload[k] = f"'{v}'"
+            # remove id because the sql-receptionist will take care of that.
+            del payload["id"]
+            status: None | Literal['updated', 'failed'] = None
+            try:
+                with open("/run/secrets/admin", "r") as f:
+                    response = requests.post(f"{config["referenceUrls"]["db"]}/{db_name}/{parent_table_name}/{table_type}{f"/{table_name[len(parent_table_name) + 1:][:1 + len(table_type)]}" if table_type != "data" else ""}", timeout=5, cookies={
+                        "username": "admin",
+                        "password": f.read()
+                    }, json=payload)
+                    response.raise_for_status()
+            except (requests.HTTPError, requests.exceptions.RequestException) as e:
+                status = "failed"
+                num_failures += 1
+            else:
+                status="updated"
+                num_successes += 1
+            finally:
+                info_conn.execute("""
+                                  UPDATE sync_status
+                                  SET status=%s, sync_timestamp=%s
+                                  WHERE "id"=%s
+                                  """, (status, datetime.datetime.now().isoformat(), sync_status_id,))
+            target_record_cur.close()
+        targets_cur.close()
+        
+        if SYNC_VERBOSITY > 0:
+            print(f"Successfully synced {num_successes} entries and failed to sync {num_failures} entries.")
+
+        if SYNC_VERBOSITY > 1:
+            summary_cur = info_conn.execute("SELECT * FROM sync_status;")
+            print(summary_cur.fetchall())
+            summary_cur.close()
+    
+    # print("Sync complete.")
+
+def queue_sync() -> None:
+    SYNC_EVENT.set()
+
+SYNC_EVENT: threading.Event = threading.Event()
+
+AUTO_SYNC_THREAD: threading.Thread = threading.Thread(target=auto_sync, args=(SYNC_EVENT,))
+AUTO_SYNC_THREAD.start()
