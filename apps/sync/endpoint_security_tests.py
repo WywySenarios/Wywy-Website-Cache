69bb2f5b8d96fe0faa5fb29bcc6aaf827787ea5a
WywySenarios <wywyericzhu@gmail.com>
2026-04-03 21:53:51 -0400

    Add endpoint security testing

---
+++ apps/sync/endpoint_security_tests.py
@@ -0,0 +1,51 @@
+from copy import deepcopy
+from unittest import TestCase
+from requests import get as GET, post as POST
+from constants import AUTH_COOKIES, GENERIC_REQUEST_PARAMS
+
+
+def test_endpoint_security(test_object: TestCase, endpoint: str) -> None:
+    # does the endpoint check the origin header?
+    request_params = deepcopy(GENERIC_REQUEST_PARAMS)
+    request_params["headers"] = None
+    response = GET(endpoint, **request_params)
+    test_object.assertEqual(
+        response.status_code,
+        400,
+        f"Invalid (origin-less) response did not receive response with status 400: {response.status_code}: {response.text}",
+    )
+
+    # is the endpoint secured with authentication?
+    request_params = deepcopy(GENERIC_REQUEST_PARAMS)
+    request_params["cookies"] = None
+    response = POST(endpoint, **request_params)
+    test_object.assertEqual(
+        response.status_code,
+        401,
+        f"Unauthenticated request did not receive response with status 401: {response.status_code}: {response.text}",
+    )
+
+    # check if modifying authentication details invalidates the authentication
+    for key in AUTH_COOKIES:
+        request_params = deepcopy(GENERIC_REQUEST_PARAMS)
+        del request_params["cookies"][key]
+
+        response = POST(endpoint, **request_params)
+        test_object.assertEqual(
+            response.status_code,
+            403,
+            f"Invalid authentication (missing authenitcation attribute) did not receive response with status 403: {response.status_code}: {response.text}",
+        )
+
+    # @TODO missing CSRF token
+
+    for key in AUTH_COOKIES:
+        request_params = deepcopy(GENERIC_REQUEST_PARAMS)
+        request_params["cookies"][key] += " <- is now invalid"
+
+        response = POST(endpoint, **request_params)
+        test_object.assertEqual(
+            response.status_code,
+            403,
+            f"Invalid authentication (incorrect attribute) did not receive response with status 403: {response.status_code}: {response.text}",
+        )
