b51689bff8eb281814a24100d1adbcbc350c3f72
WywySenarios <wywyericzhu@gmail.com>
2026-04-04 19:58:31 -0400

    Update missing Origin expected  result

--- apps/sync/endpoint_security_tests.py
+++ apps/sync/endpoint_security_tests.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 from unittest import TestCase
-from requests import get as GET, post as POST
+from requests import post as POST
 from constants import AUTH_COOKIES, GENERIC_REQUEST_PARAMS
 
 
@@ -8,11 +8,11 @@ def test_endpoint_security(test_object: TestCase, endpoint: str) -> None:
     # does the endpoint check the origin header?
     request_params = deepcopy(GENERIC_REQUEST_PARAMS)
     request_params["headers"] = None
-    response = GET(endpoint, **request_params)
+    response = POST(endpoint, **request_params)
     test_object.assertEqual(
         response.status_code,
-        400,
-        f"Invalid (origin-less) request did not receive response with status 400: {response.status_code}: {response.text}",
+        403,
+        f"Invalid (origin-less) request did not receive response with status 403: {response.status_code}: {response.text}",
     )
 
     # is the endpoint secured with authentication?
