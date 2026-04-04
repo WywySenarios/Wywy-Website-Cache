dee564567cf3a2e57f1f741bc9f9f8cf11b7bdee
WywySenarios <wywyericzhu@gmail.com>
2026-04-03 21:53:41 -0400

    Add 401 response to auth middleware

--- apps/sync/sync/auth.py
+++ apps/sync/sync/auth.py
@@ -1,4 +1,8 @@
-from django.http import HttpRequest, HttpResponse, HttpResponseForbidden
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+    HttpResponseForbidden,
+)
 from typing import Callable
 
 
@@ -9,37 +13,30 @@ class AuthMiddleware:
         with open("/run/secrets/admin", "r") as f:
             self.password = f.read()
 
-    def check_creds(self, request: HttpRequest) -> bool:
-        """Checks if the user has admin privileges. @TODO sessions
-
-        Args:
-            request (HttpRequest): The request from the user in question.
-
-        Returns:
-            bool: Whether or not the user has admin privileges.
-        """
-        if "username" not in request.COOKIES or "password" not in request.COOKIES:
-            return False
-
-        if (
-            request.COOKIES["username"] == "admin"
-            and request.COOKIES["password"] == self.password
-        ):
-            return True
-
-        return False
-
     def __call__(self, request: HttpRequest) -> HttpResponse:
+        # @TODO session cookies
 
         # check creds. Deny access if creds are invalid.
         # The authentication endpoint does not need authentication (avoid chicken & egg scenario).
         # This includes CSRF tokens!
+
+        if request.path.startswith("/auth") or request.path.startswith("/cache/csrf"):
+            return self.get_response(request)
+
+        username_supplied: bool = "username" in request.COOKIES
+        password_supplied: bool = "password" in request.COOKIES
+        if not username_supplied:
+            if not password_supplied:
+                return HttpResponse("No credentials supplied.", status=401)
+            else:
+                return HttpResponseForbidden("Invalid credentials.")
+        if not password_supplied:
+            return HttpResponseForbidden("Invalid credentials")
+
         if (
-            request.path.startswith("/auth")
-            or request.path.startswith("/cache/csrf")
-            or self.check_creds(request)
+            request.COOKIES["username"] != "admin"
+            or request.COOKIES["password"] != self.password
         ):
-            response = self.get_response(request)
-        else:
-            response = HttpResponseForbidden("Invalid credentials.")
-        return response
+            return HttpResponseForbidden("Invalid credentials.")
+
+        return self.get_response(request)
