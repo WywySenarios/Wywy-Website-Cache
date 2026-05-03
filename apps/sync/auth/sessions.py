bd7250f82f5557307cf7fdffde4282b2530bb017
WywySenarios <wywyericzhu@gmail.com>
2026-05-03 15:16:21 -0400

    Add basic session creation & tests

---
+++ apps/sync/auth/sessions.py
@@ -0,0 +1,38 @@
+from psycopg import connect
+from constants import CONN_CONFIG
+import secrets
+import hashlib
+
+
+def create_session(username: str) -> str:
+    """Attempts to create a new user session.
+
+    A successful creation involves creating a secure token and registering it with the database.
+
+    Args:
+        username (str): The username to create a session for.
+
+    Returns:
+        str | None: None on failure, the token on success.
+    """
+    id = secrets.token_urlsafe(24)[:24]
+    secret = secrets.token_urlsafe(24)[:24]
+
+    secret_hash = hashlib.sha256(secret.encode()).hexdigest()
+
+    print(id)
+    print(secret)
+    print(secret_hash)
+
+    with connect(**CONN_CONFIG, dbname="info") as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO sessions (id, user_id, secret_hash) VALUES (%s, (SELECT id FROM users where USERNAME=%s), %s)",
+                (
+                    id,
+                    username,
+                    secret_hash,
+                ),
+            )
+
+    return f"{id}.{secret}"
--- apps/sync/auth/tests.py
+++ apps/sync/auth/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from unittest import TestCase
+from psycopg import connect
+from constants import CONN_CONFIG
+from .sessions import create_session
 
-# Create your tests here.
+
+class TestSessionCreation(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        with connect(**CONN_CONFIG, dbname="info") as conn:
+            with conn.cursor() as cur:
+                cur.execute("TRUNCATE sessions")
+
+    def testSessionCreation(self):
+        """Test whether or not session creation runs to completion and contains a potentially valid token."""
+
+        token = create_session("admin")
+
+        self.assertEqual(len(token), 24 + 1 + 24)
